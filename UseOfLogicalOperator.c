12366ed19c81f8b82c0dfcbafa4101a63a5b6e88
ankita246 <83629740+ankita246@users.noreply.github.com>
2021-05-20 22:50:21 +0530

    Create UseOfLogicalOperator.c

---
+++ UseOfLogicalOperator.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	
+	printf("enter the first number\n ");
+	scanf("%d",&a);	
+	
+	printf("enter the second number\n ");
+	scanf("%d",&b);
+	
+	
+	
+	printf("(a==b)&&(a!=b)=%d\n",(a==b)&&(a!=b));
+		printf("(a==b)&&(a>b)=%d\n",(a==b)&&(a>b));
+			printf("(a>=b)&&(a>b)=%d\n",(a>=b)&&(a>b));
+				printf("(a<=b)&&(a<b)=%d\n",(a<=b)&&(a<b));
+				
+				
+					printf("(a==b)||(a!=b)=%d\n",(a==b)||(a!=b));
+		printf("(a==b)||(a>b)=%d\n",(a==b)||(a>b));
+			printf("(a>=b)||(a>b)=%d\n",(a>=b)||(a>b));
+				printf("(a<=b)||(a<b)=%d\n",(a<=b)||(a<b));
+				
+					printf("!(a!=b)=%d\n",!(a!=b));
+		printf("!(a>b)=%d\n",!(a>b));
+			printf("!(a>=b)=%d\n",!(a>=b));
+				printf("!(a<b)=%d\n",!(a<b));
+
+	
+	
+			
+
+			
+		return 0 ;
+	
+}
+© 2021 GitHub, Inc.
